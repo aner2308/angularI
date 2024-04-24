856c40ce4145931dda0e707c6edbd645d1b8cde6
Anton <erikssonanton94@gmail.com>
2024-04-25 01:49:03 +0200

    Lagt till funktionalitet för mina konverterare

--- src/app/length-converter/length-converter.component.ts
+++ src/app/length-converter/length-converter.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-length-converter',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './length-converter.component.html',
   styleUrl: './length-converter.component.scss'
 })
 export class LengthConverterComponent {
+  inputValue: number = 0;
+  result: number = 0;
+  conversionDirection: string = "metersToFeet";
 
+  convert() {
+    if (this.conversionDirection === "metersToFeet") {
+      this.result = this.inputValue * 3.28084;
+    } else if (this.conversionDirection === "feetToMeters") {
+      this.result = this.inputValue / 3.28084;
+    }
+
+    //Avrunda till en decimal
+    this.result = parseFloat(this.result.toFixed(1)); 
+  }
 }
--- src/app/temperature-converter/temperature-converter.component.ts
+++ src/app/temperature-converter/temperature-converter.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-temperature-converter',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './temperature-converter.component.html',
   styleUrl: './temperature-converter.component.scss'
 })
 export class TemperatureConverterComponent {
+  inputValue: number = 0;
+  result: number = 0;
+  conversionDirection: string = "celsiusToFahrenheit";
 
+  convert(): void {
+    if (this.conversionDirection === "celsiusToFahrenheit") {
+      this.result = (this.inputValue * 9 / 5) + 32;
+    } else if (this.conversionDirection === "fahrenheitToCelsius") {
+      this.result = (this.inputValue - 32) * 5 / 9;
+    }
+
+    //Avrunda till en decimal
+    this.result = parseFloat(this.result.toFixed(1));
+  }
 }
