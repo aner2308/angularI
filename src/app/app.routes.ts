ce89903ef118c18b2eded5ce5afef757d3f4e29d
Anton <erikssonanton94@gmail.com>
2024-04-24 11:09:20 +0200

    Startstruktur för min webbplats skapad med tre undersidor och en huvudmeny

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ConverterPageComponent } from './converter-page/converter-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'converterPage', component: ConverterPageComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: '404', component: NotFoundComponent},
+    {path:'**', component: NotFoundComponent}
+
+];
