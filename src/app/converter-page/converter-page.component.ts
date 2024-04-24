4c8fc98c8972e567847e842ba68b21e3b9448057
Anton <erikssonanton94@gmail.com>
2024-04-25 00:56:21 +0200

    Lagt till vägar till mina konverterare

--- src/app/converter-page/converter-page.component.ts
+++ src/app/converter-page/converter-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { LengthConverterComponent } from '../length-converter/length-converter.component';
+import { TemperatureConverterComponent } from '../temperature-converter/temperature-converter.component';
 
 @Component({
   selector: 'app-converter-page',
   standalone: true,
-  imports: [],
+  imports: [LengthConverterComponent, TemperatureConverterComponent],
   templateUrl: './converter-page.component.html',
   styleUrl: './converter-page.component.scss'
 })
